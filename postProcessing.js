5c75846610f8cbb9bc10d5602a9c78e286ee84df
kjgarza <kgarza@datacite.org>
2022-07-25 15:24:14 +0200

    post process

---
+++ postProcessing.js
@@ -0,0 +1,12 @@
+import { readJSON, writeJSON, writeCSV } from 'https://deno.land/x/flat/mod.ts'
+
+// The filename is the first invocation argument
+const filename = Deno.args[0] // Same name as downloaded_filename
+const data = await readJSON(filename)
+
+// Pluck a specific key off
+// and write it out to a different file
+// Careful! any uncaught errors and the workflow will fail, committing nothing.
+const newfile = `${filename}`
+// await writeJSON(newfile, data.path.to.something)
+await writeCSV(newfile, data.datasets.subjects)
